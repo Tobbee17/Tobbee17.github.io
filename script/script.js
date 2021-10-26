477cc383690cfb1f089c8523319a40a9cda4adcc
Tobias Engli Fladen <tobiae1805@osloskolen.no>
2021-10-26 09:02:53 +0200

    promt

--- script/script.js
+++ script/script.js
@@ -4,12 +4,15 @@ var gammel = 67;
 
 var alder = Number(prompt("Hvor gammel er du"));
 
-if (alder == gratis && alder < 3) {
+if (alder >= 0 && alder <= 3) {
     console.log("Du kan kjøre gratis");
 } 
-else if (alder < barn) {
+else if (alder >= barn && alder <= 15) {
     console.log("Du får 50% rabbat siden du er mellom 4-15 år");
 }
 else if (alder >= gammel) {
     console.log("Du får 50% rabbat siden du er eldre en 67");
+}
+else if (alder >= 15 && alder <= gammel) {
+    console.log(" Du må betale full pris");
 }
